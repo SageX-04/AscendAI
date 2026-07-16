5f72c560c20990a84f6dc6c735eaa91e69d006c5
Ronit Thapa <ronitkrumper81@gmail.com>
2026-07-16 14:18:08 +0530

    Added CTa section

---
+++ src/CTA.jsx
@@ -0,0 +1,24 @@
+import "./CTA.css";
+
+function CTA() {
+  return (
+    <section className="cta">
+
+      <h2>
+        Ready To Build Your AI Future?
+      </h2>
+
+      <p>
+        Join AscendAI and start your journey toward
+        future-ready skills and career growth.
+      </p>
+
+      <button className="cta-btn">
+        Join AscendAI
+      </button>
+
+    </section>
+  );
+}
+
+export default CTA;
